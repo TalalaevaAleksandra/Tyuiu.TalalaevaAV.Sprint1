34c6ad608fb9f9542a8fe1c3ab13dde0d3493c15
Tyuiu.TalalaevaAV@outlook.com <Tyuiu.TalalaevaAV@outlook.com>
2024-09-21 01:43:01 +0500

    Add project files.

---
+++ Tyuiu.TalalaevaAV.Sprint1.Task0.V19/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.TalalaevaAV.Sprint1.Task0.V19.Lib;
+namespace Tyuiu.TalalaevaAV.Sprint1.Task0.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1| Выполнила: Талалаева А.В. | ИИПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Талалаева Александра Васильевна | ИИПБ-24-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на C#, которая вычисляет выражение 4/2*5/(3+2)*5     *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("* 4/2*5/(3+2)*5                                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}
