f08de794146b31c880c7e42cb95f437bd977c9f2
Tyuiu.TalalaevaAV@outlook.com <Tyuiu.TalalaevaAV@outlook.com>
2024-09-30 17:50:18 +0500

    Task1

---
+++ Tyuiu.TalalaevaAV.Sprint1.Task0.V3/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.TalalaevaAV.Sprint1.Task1.V3.Lib;
+
+namespace Tyuiu.TalalaevaAV.Sprint1.Task1.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1| Выполнила: Талалаева А.В. | ИИПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Талалаева Александра Васильевна | ИИПБ-24-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле (x-y)/(x+3)+3 и печатает его на экране.  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("* (x-y)/(x+3)+3                                                           *");
+            Console.WriteLine("***************************************************************************");
+           
+
+            double x, y;
+            Console.WriteLine("Введите значение X:");
+            x=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y= Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x,y));
+            Console.ReadLine();
+        }
+    }
+}
