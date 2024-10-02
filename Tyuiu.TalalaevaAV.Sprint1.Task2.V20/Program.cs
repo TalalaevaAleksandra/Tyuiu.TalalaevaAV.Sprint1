431bd33350ebc39a1b3484bfe40c74d0776a50c0
Tyuiu.TalalaevaAV@outlook.com <Tyuiu.TalalaevaAV@outlook.com>
2024-10-02 16:13:40 +0500

    Task2

---
+++ Tyuiu.TalalaevaAV.Sprint1.Task2.V20/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.TalalaevaAV.Sprint1.Task2.V20.Lib;
+namespace Tyuiu.TalalaevaAV.Sprint1.Task2.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1| Выполнила: Талалаева А.В. | ИИПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Талалаева Александра Васильевна | ИИПБ-24-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("* x,y- целые числа. Найти сумму квадратов                                 *");
+            Console.WriteLine("***************************************************************************");
+            int x;
+            Console.WriteLine("Введите значение X");
+            x=Convert.ToInt32(Console.ReadLine());
+            int y;
+            Console.WriteLine("Введите значение Y");
+            y= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма квадратов чисел: "+ ds.CalculateSquaresSumm(x,y));
+            Console.ReadLine();
+
+        }
+    }
+
+}
